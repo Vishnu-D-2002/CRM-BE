d11da694f4b22e10a65b16213ac59187d393a613
Vishnu-D-2002 <vishnulosangels@gmail.com>
2024-01-30 21:00:08 +0530

    profile data update

--- models/user.js
+++ models/user.js
@@ -3,6 +3,10 @@ const { default: mongoose } = require("mongoose");
 const userSchema =new mongoose.Schema({
     name: String,
     email: String,
+    aternateEmail: String,
+    address: String,
+    gender:String,
+    Contact:Number,
     passwordHash: String,
     activationToken: String,
     activated: { type: Boolean, default: false },
